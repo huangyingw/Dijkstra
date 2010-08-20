f027a86ddaac27335e90468f006ad8512a0e595d
huangyingw@vsts_2008_chs <huangyingw@gmail.com>
2010-08-19 21:32:24 -0400

    n

--- Dijkstra/Dijkstra.h
+++ Dijkstra/Dijkstra.h
@@ -3,22 +3,34 @@
 
 #include "stdafx.h"
 #include "stack.h"
-#include "Dijkstra.h"
 #include <stdio.h>
 #include<iostream>
 using namespace std;
 
-#define MAXNUM 9999
 #define MAXVERTEX 20
-
+#define MAXNUM 9999
 int dist[MAXVERTEX];
 bool S[MAXVERTEX];
 int path[MAXVERTEX];
+
+
+
 
 typedef struct
 {
 	int weight;//弧的权值
 }AdjMatrix[20][20];
+
+class Dijkstra
+{
+	private:
+
+	public:
+		int* data;
+		Dijkstra();
+		~Dijkstra();
+		
+};
 
 
 typedef struct				//图的邻接矩阵形式定义
@@ -153,45 +165,3 @@ void BellmanFord(AdjMGraph_L gra,int n,int v) //在带权有向图中有的边具有负的权值
 		}
 	}
 }
-
-int _tmain(int argc, _TCHAR* argv[])
-{
-	int preVertex;
-	int nextVertex;
-	AdjMGraph_L G;
-	int data[7][7]={{MAXNUM,28,MAXNUM,MAXNUM,MAXNUM,10,MAXNUM},
-					{28,MAXNUM,16,MAXNUM,MAXNUM,MAXNUM,14},
-					{MAXNUM,16,MAXNUM,12,MAXNUM,MAXNUM,MAXNUM},
-					{MAXNUM,MAXNUM,12,MAXNUM,22,MAXNUM,18},
-					{MAXNUM,MAXNUM,MAXNUM,22,MAXNUM,25,24},
-					{10,MAXNUM,MAXNUM,MAXNUM,25,MAXNUM,MAXNUM},
-					{MAXNUM,14,MAXNUM,18,24,MAXNUM,MAXNUM}};
-	CreatAdjMGraph_L(G,*data,7);//创建图用邻接矩阵表示
-	ShortestPath(G,7,0);
-	for(int i=0;i<7;i++)
-	{
-		preVertex=i;
-		SeqStack *S=new SeqStack();
-		InitStack(S);
-	
-		cout<<i<<":";
-		if(preVertex>=0)
-		{
-			Push(S,preVertex);
-		}
-		preVertex=path[preVertex];
-		while(preVertex>=0)
-		{	
-			Push(S,preVertex);
-			preVertex=path[preVertex];
-		}
-		
-		while(!StackEmpty(S))
-		{
-			cout<<Pop(S)<<",";
-		}
-		cout<<"->"<<dist[i]<<endl;
-	}
-	return 0;
-}
-
